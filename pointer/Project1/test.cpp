aeab00408dd9d6046fd76c0d2c382ae8874c7e1e
lisj <1358108248@qq.com>
2023-09-03 17:00:44 +0800

    know difference bettwon ptr and ref

--- pointer/Project1/test.cpp
+++ pointer/Project1/test.cpp
@@ -2,10 +2,45 @@
 
 using namespace std;
 
-int swap()
-{
+struct Base {
 
-	return 0;
+    int a;
+    int b;
+    int c;
+};
+
+class Base02
+{
+public:
+    Base02() {
+
+
+    }
+
+};
+
+
+void modifyVal(int x, int y)
+{
+    int tmp = x+y;
+    x = y;
+    y = tmp;
+}
+
+// 
+void modifyPtr(int* p1, int* p2)
+{
+    *p1 += *p2;
+}
+
+void modifyRef(int &x,int &y, Base &be)
+{
+    be.a = x;
+    be.b = y;
+    be.c = be.a+be.b;
+    int temp = x;
+    x = y;
+    y= temp;
 }
 
 int main()
@@ -14,5 +49,17 @@ int main()
 	a = 10, b = 20;
 
 
-	return 0;
+    modifyVal(a, b);
+    std::cout << "a = " << a << "\nb = " << b << std::endl;
+
+    
+    modifyPtr(&a, &b);
+    std::cout << "a = " << a << "\nb = " << b << std::endl;
+
+    Base be{};
+
+    modifyRef(a, b, be);
+    std::cout << "a = " << a << "\nb = " << b << std::endl;
+    std::cout << "be.a = " << be.a << "\nbe.b = " << be.b << "\nbe.c = " <<be.c<< std::endl;
+    return 0;
 }
