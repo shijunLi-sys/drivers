6bf0cd1aec1cc8b111eb120ce383c3491128e030
lisj <1358108248@qq.com>
2023-09-27 23:07:10 +0800

    tryed ifdef and struct conversion

--- pointer/ifdef/ifdef.cpp
+++ pointer/ifdef/ifdef.cpp
@@ -1,14 +1,66 @@
-#include<iostream>
+#include <iostream>
+#include "ifdef.h"
 
 using namespace std;
 
-int main()
+typedef struct change {
+
+	int a=0;
+	int changed(int tmp) {
+		return tmp += 11;
+	}
+};
+
+typedef struct keep {
+
+	int b=0;
+	int kept(int b) {
+		return b;
+	}
+};
+
+typedef struct Device
 {
-#ifdef _DEBUG
-	
-#endif // _DEBUG
+	change cge;
+	keep   kep;
+};
+
+typedef struct Context
+{
+	change cg;
+	keep kp;
+} *pContext;
 
 
+int main(){
+	int a = 10;
+	int* p = &a;
+
+	cout << sizeof(a) << " " << sizeof(p) << endl;
+	cout << sizeof(int*) << endl;
+
+	int32_t i32 = 100;
+	int32_t* pi32 = &i32;
+
+	cout << sizeof(i32)<< " " << sizeof(int32_t) << endl;
+	cout << sizeof(pi32) << " " << sizeof(int32_t*) << endl;
+
+#ifdef debug
+
+	int tmp = 1;
+	Device* dev = (Device*)malloc(sizeof(Device));
+	dev->cge.a = 11;
+	dev->kep.b = 10;
+	dev->kep.kept(tmp);
+
+	Context ctx;
+	memset(&ctx, 0, sizeof(Context));
+	pContext pctx = &ctx;
+	pctx = (pContext)dev;
+	cout << pctx->cg.changed(tmp);
+	cout << pctx->kp.b;
+
+#endif // debug
 
 
 
---
+++ pointer/ifdef/ifdef.h
@@ -0,0 +1,7 @@
+
+#define debug
+//#undef debug
+
+
+
+
